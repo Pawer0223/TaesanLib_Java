cbf096eda66dbf9758574ccd0ab7d8b08663b49a
NAME <gbgo77@naver.com>
2020-10-09 20:29:03 +0900

    permutation if modify

--- Functions/Permutation.java
+++ Functions/Permutation.java
@@ -10,7 +10,7 @@ import java.util.List;
  *  [1, 2]와 [2, 1]은 다르게 취급된다. 
  */
 public class Permutation<T> {
-	
+
 	/*
 	 * result: 결과
 	 * perResult : 완성 된 수열의 index정보를 기록하는 배열
@@ -19,13 +19,13 @@ public class Permutation<T> {
 	private List<List<T>> result = new ArrayList<>();
 	private int[] perResult;
 	private boolean[] visitPosition;
-	
+
 	/*
 	 * arr: 순열을 찾을 대상.
 	 * amount: arr에서 n개의 요소로 만들 수 있는 순열.
 	 */
 	public List<List<T>> start(T[] arr, int amount) {
-		
+
 		visitPosition = new boolean[arr.length];
 		perResult = new int[amount];
 		setPermutation(arr, amount, 0);
@@ -45,12 +45,12 @@ public class Permutation<T> {
 		}
 
 		for (int i = 0; i < arr.length; i++) {
-			if (visitPosition[i])
-				continue;
-			visitPosition[i] = true;
-			perResult[cnt] = i;
-			setPermutation(arr, amount, cnt + 1);
-			visitPosition[i] = false;
+			if (!visitPosition[i]) {
+				visitPosition[i] = true;
+				perResult[cnt] = i;
+				setPermutation(arr, amount, cnt + 1);
+				visitPosition[i] = false;
+			}
 		}
 	}
 }
