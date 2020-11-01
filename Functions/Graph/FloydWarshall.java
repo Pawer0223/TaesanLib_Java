dea14c34ff0cd40731cc28b6b3a1b8f1f8856249
NAME <gbgo77@naver.com>
2020-11-02 00:24:20 +0900

    Floydwarshall modify

--- Functions/Graph/FloydWarshall.java
+++ Functions/Graph/FloydWarshall.java
@@ -2,20 +2,23 @@ package Functions.Graph;
 
 import java.util.Arrays;
 
-import Functions.Commons.GraphCommon;
-
 
 /*
  * 플로이드 와샬 알고리즘
  * 
  * 정점 A에서 다른 정점으로가는 모든 최단거리를 구한다.
  * 
+ * 핵심은 1 -> 5로 갈때의 거리와
+ *  1 -> 2 -> 5로 갈때의 거리
+ *  1 -> 3 -> 5로 갈때의 거리
+ *  1 -> 4 -> 5로 갈때의 거리를 모두 비교해서 최단거리를 찾아간다.
+ * 
  */
+
 public class FloydWarshall {
 
-	GraphCommon g = new GraphCommon();
-	int[][] distance;
-	int INF = 101;
+	int[][] distance; // 정점[i 에서][j로 가는 최단거리] 가 기록된다.
+	int INF = 101; // 초기 값으로 i -> j로가는 최단거리는 Max다! 라고 설정해주어야 한다. 
 
 	public int[][] start(int n, int[][] arr, int inf) {
 		distance = new int[n + 1][n + 1];
@@ -35,6 +38,7 @@ public class FloydWarshall {
 		return distance;
 	}
 
+	// n은 정점의 갯수이다. 1~n까지의 정점이 있다고 가정한다.
 	private void floydWarshall(int n) {
 
 		for (int k = 1; k <= n; k++ ) {
